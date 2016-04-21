dfba69844f75d490dc3f04e1795a867927eb489b
Osman Jalloh <omudu45@hotmail.com>
2016-04-21 19:02:12 +0000

    add routing for getting all posts and send new posts

--- routes/posts.js
+++ routes/posts.js
@@ -1,7 +1,11 @@
 var express = require('express');
 var Post = require('../models/posts');
+var jwt = require('express-jwt');
 
 router = express.Router();
+
+// Implement an authentication middleware
+auth = jwt({secret: 'SECRET', userProperty: 'payload'});
 
 /* GET all posts */
 router.get('/', function(req, res) {
@@ -12,3 +16,40 @@ router.get('/', function(req, res) {
 		res.json(posts);
 	});
 });
+
+/* GET Specific post by id */
+router.get('/:post_id', auth, function(req, res) {
+	Post.findById(req.params.post_id, function(err, post) {
+		if (err) {
+			res.send(err);
+		}
+		// send json data
+		res.json(post);
+	});
+});
+
+/* SEND Posts */
+router.post('/', auth, function(req, res) {
+	// Create a Post instance
+	var post = new Post();
+
+	// Set the author
+	post.author = req.payload.username;
+
+	// Set the post attributes (come from the req.body)
+	post.content = req.body.content;
+
+	// save the post
+	post.save(function(err) {
+		if (err) {
+			res.send(err);
+		}
+		// give some success message
+		res.json({message: 'User successfully Created!'});
+	});
+
+});
+
+
+/* Exports all routes to router */
+module.exports = router;
