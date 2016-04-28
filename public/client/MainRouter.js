a9e8eb508dac4749ee83b4553b0ac41e48b2da64
Osman Jalloh <omudu45@hotmail.com>
2016-04-28 16:29:02 +0000

    add the routes for register and login

--- public/client/MainRouter.js
+++ public/client/MainRouter.js
@@ -8,6 +8,28 @@
 				controller: 'IndexCtrl',
 				controllerAs: 'vm',
 				templateUrl: '/client/templates/layout/index.html'
+			})
+			.state('register', {
+				url: '/register',
+				controller: 'RegisterCtrl',
+				controllerAs: 'vm',
+				templateUrl: '/client/templates/authentication/register.html',
+				onEnter: ['$state', 'Authentication', function($state, Authentication) {
+					if (Authentication.isAuthenticated()) {
+						$state.go('home');
+					}
+				}]
+			})
+			.state('login', {
+				url: '/login',
+				controller: 'LoginCtrl',
+				controllerAs: 'vm',
+				templateUrl: '/client/templates/authentication/login.html',
+				onEnter: ['$state', 'Authentication', function($state, Authentication) {
+					if (Authentication.isAuthenticated()) {
+						$state.go('home');
+					}
+				}]
 			});
 		$urlRouterProvider.otherwise('home');
 	}]);
